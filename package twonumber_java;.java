a3c469516798383d72e88d010e348380eef9d740
sagar01111 <charansagarc@gmail.com>
2025-01-02 14:27:06 +0530

    Add files via upload

---
+++ package twonumber_java;.java
@@ -0,0 +1,14 @@
+package twonumber_java;
+public class domon1{
+    public static void (string args[]) {
+        int num1, num2;
+        num1 = 50;
+        num2 = 100;
+        if (num1 > num2){
+            system.out.println(x:"num1 is greater ");
+        else (num2 > num1){
+            system.out.println(x:"num2 is greater ");
+        }
+        }
+    }
+}
