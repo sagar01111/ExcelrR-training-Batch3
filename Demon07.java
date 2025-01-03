080359eaefdff42d06bde257670abedda09c1664
sagar01111 <charansagarc@gmail.com>
2025-01-03 11:27:23 +0530

    name,age

---
+++ Demon07.java
@@ -0,0 +1,16 @@
+package Core_java;
+import java.util.Scanner;
+public class Demon07 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String pho;
+        System.out.println(x:"plz enter ur name,age,pho");
+        scanner sc = new scanner(System.in);
+        name = sc.nextLine();
+        age = sc.nextLine();
+        pho = sc.nextLine();
+        System.out.println("hello"name +"ur age is "+age+"ur pho num is "+pho);
+    }
+        
+}
